64d4e8ab84ef6bdc42e064704b7d566c823c3317
Sandriio <andriiskulsky@gmail.com>
2026-09-19 10:54:51 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Vite configuration for the Autdoor Actyvni app.
+// Vite configuration for the Tropa Club app.
 export default defineConfig({
   plugins: [react()],
 });
